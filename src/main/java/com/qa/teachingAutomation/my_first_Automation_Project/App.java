76d91a10983e9451a1e0d1bc006dad9f60fccf18
unknown <mar_vafa@yahoo.com>
2023-10-21 22:52:13 -0400

    first push

---
+++ src/main/java/com/qa/teachingAutomation/my_first_Automation_Project/App.java
@@ -0,0 +1,13 @@
+package com.qa.teachingAutomation.my_first_Automation_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/qa/teachingAutomation/my_first_Automation_Project/MyFirstAutomationCode.java
@@ -0,0 +1,17 @@
+package com.qa.teachingAutomation.my_first_Automation_Project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstAutomationCode {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://amazon.com");
+		System.out.println(driver.getCurrentUrl());
+		driver.quit();
+		
+	}
+
+}
